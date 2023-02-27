a3753f3e02df823963c9e9a7ead1d71b4443cdb5
@chuangda_1 <fangwanning@huawei.com>
2023-02-27 18:13:15 +0800

    modify print format --0227_5
    Signed-off-by: @chuangda_1 <fangwanning@huawei.com>

--- frameworks/kits/extension/src/js_print_extension_context.cpp
+++ frameworks/kits/extension/src/js_print_extension_context.cpp
@@ -30,7 +30,7 @@
 #include "start_options.h"
 
 using namespace OHOS::Print;
-typedef void* nonetype;
+using noneType = void*;
 
 namespace OHOS {
 namespace AbilityRuntime {
@@ -42,7 +42,7 @@ public:
     explicit JsPrintExtensionContext(const std::shared_ptr<PrintExtensionContext>& context) : context_(context) {}
     ~JsPrintExtensionContext() = default;
 
-    static void Finalizer(NativeEngine *engine, nonetype data, nonetype hint)
+    static void Finalizer(NativeEngine *engine, noneType data, noneType hint)
     {
         PRINT_HILOGD("JsAbilityContext::Finalizer is called");
         std::unique_ptr<JsPrintExtensionContext>(static_cast<JsPrintExtensionContext*>(data));
