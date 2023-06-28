6fb0675ed2178d95279b994c98be57bde4d9c191
panhui <panhui1@huawei.com>
2023-06-28 11:05:32 +0800

    增加打印企业管控 Signed-off-by:panhui1@huawei.com
    Signed-off-by: panhui <panhui1@huawei.com>

--- services/include/print_service_ability.h
+++ services/include/print_service_ability.h
@@ -95,6 +95,7 @@ private:
     void ReportHisysEvent(const std::shared_ptr<PrintJob> &jobInfo, const std::string &printerId, uint32_t subState);
     void ReportCompletedPrint(const std::string & printerId);
     void CheckJobQueueBlocked(const PrintJob &jobInfo);
+    bool CheckIsPrinterDisabled();
 
 private:
     PrintSecurityGuardManager securityGuardManager_;
