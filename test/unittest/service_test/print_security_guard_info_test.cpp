a30f3c3e405f5ef4f186551433cac2deb0dc2789
b30052170 <baozewei@huawei.com>
2024-02-06 17:16:00 +0800

    打印框架添加TDD测试用例合OH4.1release Signed-off-by:baozewei@huawei.com
    Signed-off-by: b30052170 <baozewei@huawei.com>

--- test/unittest/service_test/print_security_guard_info_test.cpp
+++ test/unittest/service_test/print_security_guard_info_test.cpp
@@ -68,6 +68,7 @@ HWTEST_F(PrintSecurityGuardInfoTest, PrintSecurityGuardInfoTest_0003, TestSize.L
     std::string option = R"({"ePrintUrl":"testUrl"})";
     printerInfo.SetOption(option);
     PrintJob printJob;
+    printJob.SetSubState(PRINT_JOB_COMPLETED_SUCCESS;)
     printSecurityGuardInfo.setPrintTypeInfo(printerInfo, printJob);
     EXPECT_NE("", printSecurityGuardInfo.ToJsonStr());
 }
@@ -81,6 +82,7 @@ HWTEST_F(PrintSecurityGuardInfoTest, PrintSecurityGuardInfoTest_0004, TestSize.L
     std::string option = R"({"ePrintUrl":0})";
     printerInfo.SetOption(option);
     PrintJob printJob;
+    printJob.SetSubState(PRINT_JOB_COMPLETED_CANCELLED;)
     printSecurityGuardInfo.setPrintTypeInfo(printerInfo, printJob);
     EXPECT_NE("", printSecurityGuardInfo.ToJsonStr());
 }
@@ -94,6 +96,7 @@ HWTEST_F(PrintSecurityGuardInfoTest, PrintSecurityGuardInfoTest_0005, TestSize.L
     std::string option = R"({"":"testUrl"})";
     printerInfo.SetOption(option);
     PrintJob printJob;
+    printJob.SetSubState(PRINT_JOB_COMPLETED_FAILED;)
     printSecurityGuardInfo.setPrintTypeInfo(printerInfo, printJob);
     EXPECT_NE("", printSecurityGuardInfo.ToJsonStr());
 }
@@ -107,6 +110,7 @@ HWTEST_F(PrintSecurityGuardInfoTest, PrintSecurityGuardInfoTest_0006, TestSize.L
     std::string option = R"({"":""})";
     printerInfo.SetOption(option);
     PrintJob printJob;
+    printJob.SetSubState(PRINT_JOB_COMPLETED_FILE_CORRUPT;)
     printSecurityGuardInfo.setPrintTypeInfo(printerInfo, printJob);
     EXPECT_NE("", printSecurityGuardInfo.ToJsonStr());
 }
