5733411d72dc9b078d32e5aaef282a3704d955d1
liangjin <liangjin16@huawei.com>
2024-09-12 18:54:13 +0800

    AI检视问题修改
    Signed-off-by: l00808155 <liangjin16@huawei.com>

--- interfaces/kits/napi/print_napi/src/print_module.cpp
+++ interfaces/kits/napi/print_napi/src/print_module.cpp
@@ -75,9 +75,7 @@ static constexpr const char *FUNCTION_GET_ADDED_PRINTER_INFO_BY_ID = "getAddedPr
 static void SetEnumProperty(napi_env env, napi_value object, const std::string &name, uint32_t value)
 {
     napi_value tempValue = nullptr;
-    if (napi_create_int32(env, value, &tempValue) != napi_ok) {
-        return;
-    }
+    napi_create_int32(env, value, &tempValue);
     napi_set_named_property(env, object, name.c_str(), tempValue);
 }
 
