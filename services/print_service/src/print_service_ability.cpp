b81c315f1abf729672fba4b7d7e1e3f7fe6a1fca
yyc1234 <leyunchi@huawei.com>
2024-10-18 02:05:23 +0000

    update services/print_service/src/print_service_ability.cpp.
    Signed-off-by: yyc1234 <leyunchi@huawei.com>

--- services/print_service/src/print_service_ability.cpp
+++ services/print_service/src/print_service_ability.cpp
@@ -2855,7 +2855,7 @@ int32_t PrintServiceAbility::RemovePrinterFromDiscovery(const std::string &print
     }
     bool mdnsPrinter = printerId.find("mdns") != string::npos;
     const uint32_t waitTime = 1000;
-    JobMonitorParam monitorParam{ nullptr, 0, 0, printerUri, "", printerId };
+    JobMonitorParam monitorParam{ nullptr, "", 0, printerUri, "", printerId };
     PRINT_HILOGD("printerid is %{public}s, printer type is %{public}d", printerId.c_str(), mdnsPrinter);
     // 连接类型为mdns且为spooler显示的已经连接的打印机才判断是否离线
     if (!printerUri.empty() && mdnsPrinter &&
