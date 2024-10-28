34b3b886292202e6202d308955962a19c6d832c1
jsy <jusiyao1@huawei.com>
2024-10-28 18:21:24 +0800

    add preload module
    Signed-off-by: jsy <jusiyao1@huawei.com>

--- test/unittest/extension_test/mock/mock_runtime.h
+++ test/unittest/extension_test/mock/mock_runtime.h
@@ -87,6 +87,16 @@ public:
     {
         return;
     }
+    void PreloadMainAbility(const std::string& moduleName, const std::string& srcPath,
+        const std::string& hapPath,  bool isEsMode, const std::string& srcEntrance) override
+    {
+        return;
+    }
+    void PreloadModule(const std::string& moduleName, const std::string& srcPath,
+        std::string& hapPath, bool isEsMode, bool useCommonTrunk) override
+    {
+        return;
+    }
     bool RunScript(const std::string& path, const std::string& hapPath, bool useCommonChunk = false)
     {
         return true;
