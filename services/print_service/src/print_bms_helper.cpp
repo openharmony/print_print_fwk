ebef83ca58745089684fba6e7f77920fc38ad13e
张智勇 <zhangzhiyong43@huawei.com>
2024-11-04 21:46:05 +0800

    修复稳定性问题
    Signed-off-by: 张智勇 <zhangzhiyong43@huawei.com>

--- services/print_service/src/print_bms_helper.cpp
+++ services/print_service/src/print_bms_helper.cpp
@@ -115,7 +115,6 @@ void PrintBMSHelper::ResetProxy(const wptr<IRemoteObject> &remote)
         PRINT_HILOGD("need reset");
         serviceRemote->RemoveDeathRecipient(printBMSDeath_);
         sptrBundleMgr_ = nullptr;
-        delete printBMSDeath_;
         printBMSDeath_ = nullptr;
     }
 }
