6abe7d23969e77d3c9ca14b82a62f82c4af1f2d0
baozewei <baozewei@huawei.com>
2025-05-15 19:31:36 +0800

    5.1.0:userId校验整改 Signed-off-by:baozewei@huawei.com
    Signed-off-by: baozewei <baozewei@huawei.com>

--- utils/include/print_constant.h
+++ utils/include/print_constant.h
@@ -277,5 +277,7 @@ const std::string VENDOR_WLAN_GROUP = "driver.wlan.group";
 const std::string VENDOR_BSUNI_DRIVER = "driver.bsuni";
 const std::string VENDOR_PPD_DRIVER = "driver.ppd";
 const std::string VENDOR_IPP_EVERYWHERE = "driver.ipp.everywhere";
+
+const int32_t INVALID_USER_ID = -1;
 } // namespace OHOS::Print
 #endif // PRINT_CONSTANT_H
