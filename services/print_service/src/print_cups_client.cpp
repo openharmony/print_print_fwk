4568202b90c8f0cc3c7445e828d26706ac1ff1e7
田迅 <tianxun3@huawei.com>
2025-07-07 14:22:01 +0800

    fix codecheck
    Signed-off-by: 田迅 <tianxun3@huawei.com>

--- services/print_service/src/print_cups_client.cpp
+++ services/print_service/src/print_cups_client.cpp
@@ -182,7 +182,7 @@ std::string GetUsbPrinterSerial(const std::string &deviceUri)
 
 static std::mutex g_usbPrintersLock;
 static std::vector<PrinterInfo> g_usbPrinters;
-std::vector<PrinterInfo> GetUsbPrinters()
+std::vector<PrinterInfo>& GetUsbPrinters()
 {
     std::lock_guard<std::mutex> lock(g_usbPrintersLock);
     return g_usbPrinters;
@@ -200,7 +200,7 @@ void ClearUsbPrinters()
 
 static std::mutex g_backendPrintersLock;
 static std::vector<PrinterInfo> g_backendPrinters;
-std::vector<PrinterInfo> GetBackendPrinters()
+std::vector<PrinterInfo>& GetBackendPrinters()
 {
     std::lock_guard<std::mutex> lock(g_backendPrintersLock);
     return g_backendPrinters;
