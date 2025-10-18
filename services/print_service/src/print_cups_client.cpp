12879ecd177f9a571cf4801764bd56fc7e3b9c9a
qq_46613531 <zhangziming10@huawei.com>
2025-10-18 15:09:51 +0800

    修复连续下发时概率弹出两次认证窗口
    Signed-off-by: qq_46613531 <zhangziming10@huawei.com>

--- services/print_service/src/print_cups_client.cpp
+++ services/print_service/src/print_cups_client.cpp
@@ -1702,6 +1702,14 @@ bool PrintCupsClient::JobStatusCallback(std::shared_ptr<JobMonitorParam> monitor
         return true;
     }
 
+    if (monitorParams->job_state == IPP_JOB_HELD) {
+        if (monitorParams->job_state_reasons == JOB_STATE_REASON_AUTHENTICATION) {
+            monitorParams->serviceAbility->UpdatePrintJobState(
+                monitorParams->serviceJobId, PRINT_JOB_BLOCKED, PRINT_JOB_BLOCKED_AUTHENTICATION);
+            return true;
+        }
+    }
+
     if (monitorParams->job_state == IPP_JOB_PENDING || monitorParams->job_state == IPP_JOB_HELD) {
         PRINT_HILOGI("job is queued");
         if (monitorParams->job_state_reasons == JOB_STATE_REASON_PRINTER_STOP) {
@@ -1709,25 +1717,9 @@ bool PrintCupsClient::JobStatusCallback(std::shared_ptr<JobMonitorParam> monitor
                 monitorParams->serviceJobId, PRINT_JOB_BLOCKED, PRINT_JOB_BLOCKED_PRINTER_UNAVAILABLE);
             return true;
         }
-        if (monitorParams->job_state_reasons == JOB_STATE_REASON_AUTHENTICATION) {
-            monitorParams->serviceAbility->UpdatePrintJobState(
-                monitorParams->serviceJobId, PRINT_JOB_BLOCKED, PRINT_JOB_BLOCKED_AUTHENTICATION);
-            return true;
-        }
         monitorParams->serviceAbility->UpdatePrintJobState(
             monitorParams->serviceJobId, PRINT_JOB_QUEUED, PRINT_JOB_COMPLETED_SUCCESS);
         return true;
-    }
-
-    if (monitorParams->job_state == IPP_JOB_STOPPED) {
-        if (CancelPrinterJob(monitorParams->cupsJobId)) {
-            PRINT_HILOGI("cancel PrinterJob because stopped");
-            return true;
-        }
-        monitorParams->serviceAbility->UpdatePrintJobState(monitorParams->serviceJobId,
-            PRINT_JOB_COMPLETED,
-            GetNewSubstate(monitorParams->substate, PRINT_JOB_COMPLETED_CANCELLED));
-        return false;
     }
     return SpecialJobStatusCallback(monitorParams);
 }
@@ -1791,6 +1783,17 @@ bool PrintCupsClient::SpecialJobStatusCallback(std::shared_ptr<JobMonitorParam> 
 {
     if (monitorParams == nullptr) {
         PRINT_HILOGE("monitor job state failed, monitorParams is nullptr");
+        return false;
+    }
+    
+    if (monitorParams->job_state == IPP_JOB_STOPPED) {
+        if (CancelPrinterJob(monitorParams->cupsJobId)) {
+            PRINT_HILOGI("cancel PrinterJob because stopped");
+            return true;
+        }
+        monitorParams->serviceAbility->UpdatePrintJobState(monitorParams->serviceJobId,
+            PRINT_JOB_COMPLETED,
+            GetNewSubstate(monitorParams->substate, PRINT_JOB_COMPLETED_CANCELLED));
         return false;
     }
 
