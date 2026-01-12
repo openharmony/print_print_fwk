a8e697ee9d207b8887a7ba92fb70ea62e18273a5
Satsukirin <1173386218@qq.com>
2026-01-12 11:33:29 +0800

    回退
    Signed-off-by: Satsukirin <1173386218@qq.com>

--- test/unittest/fwk_print_service_ability_test/print_service_ability_test.cpp
+++ test/unittest/fwk_print_service_ability_test/print_service_ability_test.cpp
@@ -2859,7 +2859,7 @@ HWTEST_F(PrintServiceAbilityTest, ConnectPrinterByIpAndPpdTest, TestSize.Level1)
     auto service = std::make_shared<PrintServiceAbility>(PRINT_SERVICE_ID, true);
     std::string ip = "192.168.1.1";
     std::string protocol = "ipp";
-    std::string ppdName = "test.ppd";
+    std::string ppdName = BSUNI_PPD_NAME;
     EXPECT_EQ(service->ConnectPrinterByIpAndPpd(ip, protocol, ppdName), E_PRINT_NONE);
     EXPECT_EQ(service->ConnectPrinterByIpAndPpd("1", protocol, ppdName), E_PRINT_INVALID_PRINTER);
 }
@@ -2879,7 +2879,7 @@ HWTEST_F(PrintServiceAbilityTest, ConnectPrinterByIdAndPpdTest, TestSize.Level1)
     auto service = std::make_shared<PrintServiceAbility>(PRINT_SERVICE_ID, true);
     std::string printerId = "testId";
     std::string protocol = "ipp";
-    std::string ppdName = "test.ppd";
+    std::string ppdName = BSUNI_PPD_NAME;
     EXPECT_EQ(service->ConnectPrinterByIdAndPpd(printerId, protocol, ppdName), E_PRINT_NO_PERMISSION);
     service->ManualStart();
     EXPECT_EQ(service->ConnectPrinterByIdAndPpd(printerId, protocol, ppdName), E_PRINT_SERVER_FAILURE);
